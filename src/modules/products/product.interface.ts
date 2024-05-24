5c9b6606b4f0fdba9bdc2b2be7dd8b41ed59b0ed
Sumiyaakhi <sumiya.akhi793@gmail.com>
2024-05-24 22:41:39 +0600

    product added

---
+++ src/modules/products/product.interface.ts
@@ -0,0 +1,20 @@
+export type TVariant = {
+  type: string;
+  value: string;
+};
+
+// Type for the inventory
+export type TInventory = {
+  quantity: number;
+  inStock: boolean;
+};
+
+export type TProduct = {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  variants: TVariant[];
+  inventory: TInventory;
+};
