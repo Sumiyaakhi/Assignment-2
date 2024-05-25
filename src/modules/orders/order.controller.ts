47c54e30ae11683a08ee8fecd64640ef20b14232
Sumiyaakhi <sumiya.akhi793@gmail.com>
2024-05-25 12:05:13 +0600

    after creating order the product data is updated

--- src/modules/orders/order.controller.ts
+++ src/modules/orders/order.controller.ts
@@ -1,15 +1,64 @@
 import { Request, Response } from "express";
 import { OrderServices } from "./order.service";
+import { ProductServices } from "../products/product.service";
 
 // create order
+// const createOrder = async (req: Request, res: Response) => {
+//   const OrderData = req.body;
+//   const result = await OrderServices.createOrder(OrderData);
+//   res.json({
+//     success: true,
+//     message: "Order created successfully!",
+//     data: result,
+//   });
+// };
+
+// creating order and also update the product data stock and quantity.....
 const createOrder = async (req: Request, res: Response) => {
-  const OrderData = req.body;
-  const result = await OrderServices.createOrder(OrderData);
-  res.json({
-    success: true,
-    message: "Order created successfully!",
-    data: result,
-  });
+  try {
+    const { email, productId, price, quantity } = req.body;
+    const product = await ProductServices.getSingleProduct(productId);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found.",
+      });
+    }
+
+    if (quantity > product.inventory.quantity) {
+      return res.status(400).json({
+        success: false,
+        message: "Insufficient stock.",
+      });
+    }
+
+    product.inventory.quantity -= quantity;
+
+    product.inventory.inStock = product.inventory.quantity > 0;
+
+    await ProductServices.updateSingleProduct(productId, product);
+
+    // Create new order
+    const order = await OrderServices.createOrder({
+      email,
+      productId,
+      price,
+      quantity,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Order created successfully!",
+      data: order,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while creating the order.",
+      error: err.message,
+    });
+  }
 };
 
 // Get all orders or search orders by email
